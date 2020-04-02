9f7c09c6c045678de6b23851943212affe9c51d5
NickJWhite12 <mr.nicholasjameswhite@gmail.com>
2020-04-02 02:41:37 -0400

    adding POJOs. Closes #2.

---
+++ src/main/java/com/nick12white/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.nick12white.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/nick12white/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.nick12white.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
