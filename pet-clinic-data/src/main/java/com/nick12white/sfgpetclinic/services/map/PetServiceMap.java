6d7f1233800b40c7e535c684b7b22828ebaac04e
NickJWhite12 <48346435+NickJWhite12@users.noreply.github.com>
2020-08-28 03:05:57 -0400

    Added static resources Closes #17

--- pet-clinic-data/src/main/java/com/nick12white/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/nick12white/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,13 +1,13 @@
 package com.nick12white.sfgpetclinic.services.map;
 
 import com.nick12white.sfgpetclinic.model.Pet;
-import com.nick12white.sfgpetclinic.services.CrudService;
+import com.nick12white.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
