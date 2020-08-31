4ca3ce49532d69f1c49b98c73c2237b011c35a44
NickJWhite12 <48346435+NickJWhite12@users.noreply.github.com>
2020-08-31 05:07:11 -0400

    Added Specialties Map imple. closes #31

---
+++ pet-clinic-data/src/main/java/com/nick12white/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.nick12white.sfgpetclinic.services;
+
+import com.nick12white.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
